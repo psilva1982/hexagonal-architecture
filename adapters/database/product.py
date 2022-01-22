6c056611dec5e710eb1444c45fe256dd960a5106
Paulo <paulo.silva@conceptu.ind.br>
2022-01-22 15:47:37 -0300

    deta

--- adapters/database/product.py
+++ adapters/database/product.py
@@ -1,16 +1,37 @@
 
+from math import prod
 from application.interface import ProductInterface
+from application.product import Product
 from application.services.interface import ProductPersistenceInterface
+from decouple import config
+from deta import Deta
 
 
 class ProductDb(ProductPersistenceInterface):
 
     def __init__(self) -> None:
-        self.project_key = 'b0566nxt_qR5eySrD4j5uzj5BrAubX1srd5PG6Ss3'
-        self.project_id = ''
+        project_key = config('DB_KEY')
+        deta = Deta(project_key)
+        self.products = deta.Base("users")
 
     def get(self, id: str) -> ProductInterface:
-        pass
+        fetch_res = self.products.fetch({"id": id})
+        fetch = fetch_res.items[0]
+        product = Product(
+            id=fetch['id'],
+            name=fetch['name'],
+            price=fetch['price'],
+            status=fetch['status']
+        )
 
-    def save(self, product: ProductInterface):
-        pass
+        return product
+
+    def save(self, product: ProductInterface)-> ProductInterface:
+        self.products.insert({
+           "id": str(product.id),
+           "name": product.name,
+           "price": product.price,
+           "status": product.status
+        })
+
+        return product
